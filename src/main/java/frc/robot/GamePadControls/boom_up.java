2714218d4aabb7891733c4dd4cbbe7186f923376
Thatcooldevguy <tsommers150@gmail.com>
2019-02-12 19:28:11 -0500

    Importing boom_up.java

---
+++ src/main/java/frc/robot/GamePadControls/boom_up.java
@@ -0,0 +1,18 @@
+//imporant packages/imports 
+package frc.robot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.*;
+import frc.robot.GamePadControls.*;
+
+@Override
+public void controlPRESETS() {
+
+}
+
+//code status: STUCK. due to error. 
+
+//error: [Java] The declared package "frc.robot" does not match the expected package "frc.robot.GamePadControls" [536871240]
+//frc
+//Please Fix In the github fork or comment of code.
